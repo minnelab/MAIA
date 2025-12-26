d044d9b91a4abca9538cdb1683839ad5854484a0
Simone Bendazzoli <simben@kth.se>
2025-12-26 07:02:19 +0000

    Enhance Dockerfile and scripts for MAIA dashboard development
    - Added a new argument for MAIA dashboard path in the Dockerfile.
    - Introduced a development start script (`start-script_dev.sh`) for setting up the MAIA environment.
    - Updated the main start script to install `maia-toolkit` and removed redundant git clone commands.
    - Implemented conditional logic in the Dockerfile to clone the MAIA repository and set up the development environment based on the DEVEL argument.

--- dashboard/start-script_dev.sh
+++ dashboard/start-script_dev.sh
@@ -2,8 +2,17 @@
 
 helm repo add maia https://minnelab.github.io/MAIA/
 helm repo update
-pip install maia-toolkit
+git clone https://github.com/minnelab/MAIA.git
+pip install ./MAIA
 
+
+cd ./MAIA
+git checkout $DEV_BRANCH
+git pull
+
+git config --global user.email $GIT_EMAIL
+git config --global user.name $GIT_NAME
+gpg --import $GPG_KEY
 
 python manage.py makemigrations authentication
 python manage.py makemigrations gpu_scheduler
