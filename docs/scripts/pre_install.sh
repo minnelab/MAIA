93e91ccc1e45095aa75abbbfa0b0ba7309768130
Simone Bendazzoli <simben@kth.se>
2026-01-09 09:40:50 +0000

    Comment out tutorial copying commands in pre_install.sh to prevent execution during installation process

--- docs/scripts/pre_install.sh
+++ docs/scripts/pre_install.sh
@@ -1,12 +1,15 @@
 cp README.md docs/source/README.md
-mkdir -p docs/source/apidocs/tutorials/Admin
-cp GPU_Booking_System.md docs/source/apidocs/tutorials/Admin/GPU_Booking_System.md
-cp GPU-Booking-System.png docs/source/apidocs/tutorials/Admin/GPU-Booking-System.png
-cp Deploy_Custom_App.md docs/source/apidocs/tutorials/Admin/Deploy_Custom_App.md
-cp Deploy_MAIA_Namespace_from_CLI.md docs/source/apidocs/tutorials/Admin/Deploy_MAIA_Namespace_from_CLI.md
-cp CIFS/README.md docs/source/apidocs/tutorials/Admin/README_CIFS.md
-cp -r dashboard/docs docs/source/apidocs/tutorials/Dashboard
-cp -r docker/MAIA-Workspace/Tutorials docs/source/apidocs/tutorials/MAIA-Workspace
+
+#mkdir -p docs/source/apidocs/tutorials/Admin
+#cp GPU_Booking_System.md docs/source/apidocs/tutorials/Admin/GPU_Booking_System.md
+#cp GPU-Booking-System.png docs/source/apidocs/tutorials/Admin/GPU-Booking-System.png
+#cp Deploy_Custom_App.md docs/source/apidocs/tutorials/Admin/Deploy_Custom_App.md
+#cp Deploy_MAIA_Namespace_from_CLI.md docs/source/apidocs/tutorials/Admin/Deploy_MAIA_Namespace_from_CLI.md
+#cp CIFS/README.md docs/source/apidocs/tutorials/Admin/README_CIFS.md
+
+#cp -r dashboard/docs docs/source/apidocs/tutorials/Dashboard
+#cp -r docker/MAIA-Workspace/Tutorials docs/source/apidocs/tutorials/MAIA-Workspace
+
 mkdir -p docs/source/apidocs/tutorials/Installation
 cp ansible/MAIA/Installation/README.md docs/source/apidocs/tutorials/Installation/README-main.md
 cp ansible/MAIA/Installation/docs/docsite/rst/main.rst docs/source/apidocs/tutorials/Installation/main.rst
