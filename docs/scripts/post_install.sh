7a301c0d6774c107e580728a753fbde5d0b8b595
Simone Bendazzoli <simben@kth.se>
2026-01-09 11:19:52 +0000

    Enhance post-install script to handle pre-release versions of maia-toolkit
    - Updated version matching regex to support pre-release versions.
    - Added conditional installation for pre-release versions using the --pre flag.

--- docs/scripts/post_install.sh
+++ docs/scripts/post_install.sh
@@ -6,8 +6,13 @@ if [[ "$READTHEDOCS_VERSION" == "latest" ]]; then
     pip install git+https://github.com/minnelab/MAIA.git
 fi
 
-if [[ "$READTHEDOCS_VERSION" =~ ^v([0-9]+\.[0-9]+\.[0-9]+) ]]; then
+if [[ "$READTHEDOCS_VERSION" =~ ^v([0-9]+\.[0-9]+\.[0-9]+(-[a-zA-Z]+\.[0-9]+)?) ]]; then
     VERSION="${BASH_REMATCH[1]}"
-    pip install "maia-toolkit==${VERSION}"
+    if [[ "$VERSION" == *-* ]]; then
+        # version is a pre-release (e.g. 1.2.3-alpha.2)
+        pip install --pre "maia-toolkit==${VERSION}"
+    else
+        pip install "maia-toolkit==${VERSION}"
+    fi
 fi
 
