2bd1aa4399c0d66e3f4282b0e6e342e40aee790f
Simone Bendazzoli <simben@kth.se>
2026-03-17 20:57:41 +0000

    Add NGINX configuration and startup script for NVFlare dashboard
    - Introduced a new NGINX configuration template in `default.template` to manage server settings and CORS policies.
    - Added a startup script `start.sh` to configure NGINX and handle environment variable substitution.
    - Updated the Dockerfile to copy the new script and template, and set the entry point to the startup script for proper initialization.

---
+++ docker/Flare-Dashboard/start.sh
@@ -0,0 +1,10 @@
+#!/bin/bash -e
+
+
+sed -i 's|worker_processes .*|worker_processes 1;|' /etc/nginx/nginx.conf
+
+envsubst '${INGRESS_PATH} < /etc/default.template > default
+mv default /etc/nginx/sites-enabled/
+nginx -c /etc/nginx/nginx.conf -g 'daemon off;' &
+
+exec "$@" 
