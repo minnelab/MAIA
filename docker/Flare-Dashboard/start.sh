0d1f4e6342d1903380288fd8491856dd9dbb23ec
Simone Bendazzoli <simben@kth.se>
2026-03-18 07:04:56 +0000

    Enhance NGINX configuration for NVFlare dashboard
    - Added support for WebSocket connections and improved header management in the NGINX configuration template.
    - Implemented HTML payload rewriting to adjust asset paths based on the ingress path.
    - Updated the startup script to correctly substitute the ingress path variable in the NGINX configuration.

--- docker/Flare-Dashboard/start.sh
+++ docker/Flare-Dashboard/start.sh
@@ -3,7 +3,7 @@
 
 sed -i 's|worker_processes .*|worker_processes 1;|' /etc/nginx/nginx.conf
 
-envsubst '${INGRESS_PATH} < /etc/default.template > default
+envsubst '${INGRESS_PATH}' < /etc/default.template > default
 mv default /etc/nginx/sites-enabled/
 nginx -c /etc/nginx/nginx.conf -g 'daemon off;' &
 
