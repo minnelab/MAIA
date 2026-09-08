be5550eab7e5dd3fe13186274670e089955c4fc3
Simone Bendazzoli <simben@kth.se>
2026-09-08 12:15:09 +0200

    Refactor loginapp repository URL formatting for improved readability
    - Enhanced the formatting of the `repo_url` assignment in the `create_loginapp_values` function for better clarity and maintainability.

--- MAIA/maia_core.py
+++ MAIA/maia_core.py
@@ -1182,7 +1182,9 @@ def create_loginapp_values(config_folder, project_id, cluster_config_dict):
     """
     loginapp_values = {
         "namespace": "authentication",
-        "repo_url": os.environ.get("MAIA_PRIVATE_REGISTRY", "https://minnelab.github.io/MAIA/"), #"https://storage.googleapis.com/loginapp-releases/charts/",
+        "repo_url": os.environ.get(
+            "MAIA_PRIVATE_REGISTRY", "https://minnelab.github.io/MAIA/"
+        ),  # "https://storage.googleapis.com/loginapp-releases/charts/",
         "chart_name": "loginapp",
         "chart_version": loginapp_chart_version,
     }
